3de4ed2193037877b9cfb4cf21ed97ab26526936
SamaraSilvia81 <samarasilvia3110@gmail.com>
2024-04-18 20:01:57 -0300

    mod

---
+++ express-01/src/routes/index.js
@@ -0,0 +1,23 @@
+import session from "./session";
+import user from "./user";
+import message from "./message";
+
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+const models = {
+  User: getUserModel(sequelize, Sequelize),
+  Message: getMessageModel(sequelize, Sequelize),
+};
+
+Object.keys(models).forEach((key) => {
+  if ("associate" in models[key]) {
+    models[key].associate(models);
+  }
+});
+
+export const routes = {
+  session,
+  user,
+  message,
+};
